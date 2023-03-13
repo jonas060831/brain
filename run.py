949b0aa7bad492e65e628bcda20966c1429c1f97
jonas060831 <jonas_sulit2001@yahoo.com>
2023-03-13 00:32:50 -0700

    testing producton build 2

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ load_dotenv(find_dotenv())
 
 if __name__ == "__main__":
     if os.getenv('PYTHON_ENV') != 'DEVELOPMENT': #WHEN on production use this
-        print(f"Server is now up and Running on port {port=os.getenv('PORT')}")
+        print(f"Server is now up and Running on port {os.getenv('PORT')}")
         serve(app, host="0.0.0.0", port=os.getenv('PORT'))
     else:
         app.run(port=8000, debug=True)
